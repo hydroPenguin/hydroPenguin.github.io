4aece457e17ae6614e10493480644296228e6315
Omegalul <fwulunch@gmail.com>
2025-01-12 12:04:40 -0800

    chore: cleanup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactGA from 'react-ga';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// Initialize Google Analytics with the tracking ID from the environment variable
+ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
+
+// Log page views
+const logPageView = () => {
+    ReactGA.set({ page: window.location.pathname });
+    ReactGA.pageview(window.location.pathname);
+};
+
+// Call logPageView on every route change
+logPageView();
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
